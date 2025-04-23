c127a0c390a0b346c7b110459585fbc8e4f5bc23
romain-pattyn <romain.pattyn.ent@gmail.com>
2025-04-23 21:00:25 +0200

    Refactor dev.nix and idx-template.nix to streamline Flutter setup by removing direct installation commands and utilizing FVM for dependency management. Enhance the bootstrap process with necessary installations and ensure consistent project creation using FVM.

--- dev.nix
+++ dev.nix
@@ -21,21 +21,7 @@
       # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
         installDependencies = ''
-          mkdir -p $HOME/.local/bin/
-          curl -fsSL https://raw.githubusercontent.com/leoafarias/fvm/e04a1f455c4db33c4c220a5239acb76c0e132c02/scripts/install.sh | bash
-          
-          export PATH="$HOME/.local/bin:$HOME/.pub-cache/bin:$PATH"
-          echo 'export PATH="$HOME/.local/bin:$HOME/.pub-cache/bin:$PATH"' >> $HOME/.bashrc
-
-          fvm global 3.29.2 -f
-          fvm flutter --version
-
-          fvm dart pub global activate mason_cli
-          fvm dart pub global activate flutterfire_cli
-
-          curl -fsSL https://install.codika.dev/install | bash
-
-          curl --proto '=https' --tlsv1.2 https://raw.githubusercontent.com/shorebirdtech/install/main/install.sh -sSf | bash
+          fvm flutter pub get
         '';
         build-flutter = ''
           cd /home/user/myapp/android
--- idx-template.nix
+++ idx-template.nix
@@ -5,12 +5,32 @@
         pkgs.xz
         pkgs.git
         pkgs.busybox
-        # Add Flutter SDK directly instead of relying on FVM initially
-        pkgs.flutter
     ];
     bootstrap = ''
+        # Install FVM
+        mkdir -p $HOME/.local/bin/
+        curl -fsSL https://raw.githubusercontent.com/leoafarias/fvm/e04a1f455c4db33c4c220a5239acb76c0e132c02/scripts/install.sh | bash
+        
+        # Add FVM to PATH
+        export PATH="$HOME/.local/bin:$HOME/.pub-cache/bin:$PATH"
+        echo 'export PATH="$HOME/.local/bin:$HOME/.pub-cache/bin:$PATH"' >> $HOME/.bashrc
+
+        # Install Flutter
+        fvm global 3.29.2 -f
+        fvm flutter --version
+
+        # Install Mason and Flutterfire
+        fvm dart pub global activate mason_cli
+        fvm dart pub global activate flutterfire_cli
+
+        # Install Codika CLI
+        curl -fsSL https://install.codika.dev/install | bash
+
+        # Install Shorebird CLI
+        curl --proto '=https' --tlsv1.2 https://raw.githubusercontent.com/shorebirdtech/install/main/install.sh -sSf | bash
+
         # Create Flutter project directly using the Nix-provided Flutter
-        flutter create "$out"
+        fvm flutter create "$out"
         mkdir -p "$out"/.idx
         cp ${./dev.nix} "$out"/.idx/dev.nix
         install --mode u+rw ${./dev.nix} "$out"/.idx/dev.nix
