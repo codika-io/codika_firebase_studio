ef4dd1ecbcea1dd8bcb569e5a61f07ead88fecce
romain-pattyn <romain.pattyn.ent@gmail.com>
2025-04-23 16:22:51 +0200

    Update idx-template.json and idx-template.nix to rename the template from 'Flutter' to 'Codika', change the publisher name, and simplify the bootstrap command in the Nix file.

--- idx-template.nix
+++ idx-template.nix
@@ -8,7 +8,7 @@
         pkgs.flutter
     ];
     bootstrap = ''
-        flutter create "$out" --template="${template}" --platforms="${platforms}" ${if sample == "none" then "" else "--sample=${sample}"} ${if blank then "-e" else ""}
+        flutter create "$out"
         mkdir "$out"/.idx
         cp ${./dev.nix} "$out"/.idx/dev.nix
         install --mode u+rw ${./dev.nix} "$out"/.idx/dev.nix
